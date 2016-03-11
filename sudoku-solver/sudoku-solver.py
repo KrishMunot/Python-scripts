7048fdd1e23edf91b35beb455d06b03107fce118
Krish Munot <krishmunot@gmail.com>
2016-03-10 16:33:29 -0800

    Update sudoku-solver.py

--- sudoku-solver/sudoku-solver.py
+++ sudoku-solver/sudoku-solver.py
@@ -1,1 +1,11 @@
-#new file here
+class sudoku:
+    def __init__(self):
+        self.maze=[]
+        self.color=[]
+        self.maxcolor=1
+        self.vertex=[]
+        self.answer=[1,2,3,4,5,6,7,8,9]
+        self.edges={}
+        for i in range(0,9):
+            self.maze+=[['_','_','_','_','_','_','_','_','_']]           
+        file=open("test.txt")
